f570fa4956a3ff23ee5e71dec5a8066373200983
Shelby Williams <shelbywayne@ou.edu>
2020-09-01 17:18:30 -0500

    #winning

---
+++ Participation/Participation3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
